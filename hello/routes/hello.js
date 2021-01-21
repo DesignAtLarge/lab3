946a240f50bec204711638847f96d4385ded30b6
leestanley <stanleylee91776@gmail.com>
2021-01-20 23:28:27 -0800

    owo ���

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -1,10 +1,14 @@
-
 /*
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function (req, res) {
+  var nameToShow = req.params.userName;
+  if (nameToShow === undefined) {
+    nameToShow = 'World';
+  }
+  console.log('Name is ' + nameToShow);
   res.render('index', {
-  	'name': '',
+    name: nameToShow,
   });
 };
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,34 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+  $('#testjs').click(function (e) {
+    $('.jumbotron h1').text('JavaScript has taken control.');
+    $('#testjs').text('Please wait... 🐱🐱🐱');
+    $('.jumbotron p').addClass('active');
+  });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+  $('a.thumbnail').click(projectClick);
+}
+
+function projectClick(e) {
+  e.preventDefault();
+  var containingProject = $(this).closest('.project');
+  var description = $(containingProject).find('.project-description');
+  if (description.length == 0) {
+    $(containingProject).append(
+      "<div class='project-description'><p>Description of the project.</p></div>"
+    );
+  } else {
+    description.html(
+      '<p>Stop clicking on me! You just did it at ' + new Date() + '</p>'
+    );
+  }
 }
